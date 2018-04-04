0b2624e7e52b38e989f883e9467d383f9b6c9f92
Jaedon Tan <naseelemak@gmail.com>
2018-04-04 15:37:48 +0800

    Added and modified a bunch more.

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "maersk";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+?>
