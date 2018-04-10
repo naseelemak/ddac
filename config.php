303a2e95d269d235ecc93600263719f498e53c61
Jaedon Tan <naseelemak@gmail.com>
2018-04-10 18:54:31 +0800

    Updated config file for Azure again again.

--- config.php
+++ config.php
@@ -5,7 +5,7 @@ session_start();
 $servername = "ddac-maersksql.mysql.database.azure.com";
 $username = "maerskmaster@ddac-maersksql";
 $password = "M@ersk121";
-$dbname = "ddac-maersksql";
+$dbname = "maersk";
 
 // Create connection
 $conn = mysqli_connect($servername, $username, $password, $dbname);
