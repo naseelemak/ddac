b1fb4e0b9c30d0f893d86e1de1fa17cd8049b7c6
Jaedon Tan <naseelemak@gmail.com>
2018-04-10 19:38:48 +0800

    Updated login page. Moved PHP before HTML.

--- login.php
+++ login.php
@@ -7,61 +7,7 @@ include 'header.php';
 if (isset($_SESSION['user'])) {
     echo "<script>window.location.href='loggedin/agent.php'</script>";
     die;
-}
-
-?>
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container">
-        <a class="navbar-brand mr-5" href="index.php">Maersk</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-            <div class="my-2 my-lg-0">
-                <!-- Insert navbar buttons here -->
-            </div>
-        </div>
-    </div>
-</nav>
-
-<div class="container mt-3">
-    <br><br>
-    <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-            <h2 class="text-center"><strong>Login</strong></h2>
-            <hr>
-            <form id="loginForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
-
-                <!-- TP Number -->
-                <div class="form-group">
-                    <input id="loginID" name="loginID" type="text" class="form-control" placeholder="Username">
-                </div>
-
-                <!-- Password -->
-                <div class="form-group">
-                    <input id="loginPass" name="loginPass" type="password" class="form-control" placeholder="Password">
-                </div>
-
-                <!-- Login Button -->
-                <input name="loginSubmit" type="submit" class="btn btn-primary btn-block mb-2 btnPointer" value="Login">
-
-        </div>
-        <div class="col-md-4"></div>
-    </div>
-</div>
-
-
-<?php
-
-// Login
-if (isset($_POST['loginSubmit'])) {
+} else if (isset($_POST['loginSubmit'])) {
     if (!empty($_POST['loginID']) && !empty($_POST['loginPass'])) {
         $username = test_input($_POST['loginID']);
         $username = strtolower($username);
@@ -115,6 +61,57 @@ if (isset($_POST['loginSubmit'])) {
         echo "window.location.replace('login.php');</script>";
     }
 }
+
+?>
+
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container">
+        <a class="navbar-brand mr-5" href="index.php">Maersk</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav mr-auto">
+
+            </ul>
+            <div class="my-2 my-lg-0">
+                <!-- Insert navbar buttons here -->
+            </div>
+        </div>
+    </div>
+</nav>
+
+<div class="container mt-3">
+    <br><br>
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4">
+            <h2 class="text-center"><strong>Login</strong></h2>
+            <hr>
+            <form id="loginForm" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>">
+
+                <!-- TP Number -->
+                <div class="form-group">
+                    <input id="loginID" name="loginID" type="text" class="form-control" placeholder="Username">
+                </div>
+
+                <!-- Password -->
+                <div class="form-group">
+                    <input id="loginPass" name="loginPass" type="password" class="form-control" placeholder="Password">
+                </div>
+
+                <!-- Login Button -->
+                <input name="loginSubmit" type="submit" class="btn btn-primary btn-block mb-2 btnPointer" value="Login">
+
+        </div>
+        <div class="col-md-4"></div>
+    </div>
+</div>
+
+
+<?php
 
 // Cleans input
 function test_input($data)
